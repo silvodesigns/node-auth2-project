d633c96de5e9ae7311b697c3d75a2967a6c90ba5
Kevin Silvestre <kevsilvo@gmail.com>
2020-06-09 16:11:58 -0400

    register with hash

--- auth/auth_router.js
+++ auth/auth_router.js
@@ -1,9 +1,27 @@
 const express = require('express');
-
+const bcrypt = require('bcryptjs');
 const router = express.Router();
+
+const Users = require('../users/users_model.js');
+
 
 router.get('/', (req, res) => {
     res.status(200).send('Hello from the auth endpoint');
+});
+
+router.post('/register', (req, res) => {
+    const newUser = req.body;
+    const hash = bcrypt.hashSync(newUser.password, 10);
+    newUser.password = hash;
+    
+    Users.add(newUser)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+
 });
 
 
