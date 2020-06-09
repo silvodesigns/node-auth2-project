4a11ff2f11c51f2e621f1097704225b7b8a7b0ad
Kevin Silvestre <kevsilvo@gmail.com>
2020-06-09 16:20:18 -0400

    login & jsonTokens

--- auth/auth_router.js
+++ auth/auth_router.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets.js');
 const router = express.Router();
 
 const Users = require('../users/users_model.js');
@@ -13,7 +15,7 @@ router.post('/register', (req, res) => {
     const newUser = req.body;
     const hash = bcrypt.hashSync(newUser.password, 10);
     newUser.password = hash;
-    
+
     Users.add(newUser)
         .then(saved => {
             res.status(201).json(saved);
@@ -23,6 +25,47 @@ router.post('/register', (req, res) => {
         });
 
 });
+
+router.post('/login', (req, res) => {
+    let { username, password } = req.body;
+
+  
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+          res.status(200).json({
+            message: `Welcome ${user.username}!`,
+            token
+          });
+        } else {
+          res.status(401).json({ message: 'You shall not pass' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+
+
+
+
+  function generateToken(user) {
+    const payload = {
+      subject: user.id, // sub in payload is what the token is about
+      username: user.username,
+      // ...otherData
+    };
+  
+    const options = {
+      expiresIn: '1d', // show other available options in the library's documentation
+    };
+  
+    // extract the secret away so it can be required and used where needed
+    return jwt.sign(payload, secrets.jwtSecret, options); // this method is synchronous
+  }
 
 
 module.exports = router;
